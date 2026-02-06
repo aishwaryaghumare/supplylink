60ba1130588c81a796976c61815fdaa6def56d61
aishwaryaghumare <aishwaryaghumare2003@gmail.com>
2026-02-06 17:49:50 +0530

    Update SupplierRepository.java

--- server/src/main/java/com/edutech/progressive/repository/SupplierRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/SupplierRepository.java
@@ -1,5 +1,12 @@
 package com.edutech.progressive.repository;
 
+import com.edutech.progressive.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface SupplierRepository {
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+
+    Supplier findBySupplierId(@Param("supplierId") int supplierId);
+
+    void deleteBySupplierId(@Param("supplierId") int supplierId);
 }
