2f9b8b4293107522343c53733337c0593352c835
aishwaryaghumare <aishwaryaghumare2003@gmail.com>
2026-02-06 17:50:42 +0530

    Update WarehouseRepository.java

--- server/src/main/java/com/edutech/progressive/repository/WarehouseRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/WarehouseRepository.java
@@ -1,5 +1,7 @@
 package com.edutech.progressive.repository;
 
+import com.edutech.progressive.entity.Warehouse;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WarehouseRepository {
+public interface WarehouseRepository extends JpaRepository<Warehouse, Integer> {
 }
