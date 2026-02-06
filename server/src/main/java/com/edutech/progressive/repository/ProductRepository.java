b8ad627e963cfd461ff1bd4d471a98229148d574
aishwaryaghumare <aishwaryaghumare2003@gmail.com>
2026-02-06 17:48:56 +0530

    Update ProductRepository.java

--- server/src/main/java/com/edutech/progressive/repository/ProductRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.edutech.progressive.repository;
 
+import com.edutech.progressive.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
